ccce1f1eab3ea7998505d5f4256417b0f4514c44
Uma Maheswari Paladugu <umamaheswari6336@gmail.com>
2022-06-05 11:54:39 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = terraform.workspace == "default" ? "us-east-1" : "us-west-2"
 }
 
-#Get Linux AMI ID using SSM Parameter endpoint in us-east-1
+
 data "aws_ssm_parameter" "linuxAmi" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-#Create and bootstrap EC2 in us-east-1
+
 resource "aws_instance" "ec2-vm" {
   ami                         = data.aws_ssm_parameter.linuxAmi.value
   instance_type               = "t2.micro"
