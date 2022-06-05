2d0e8b3aaf4bda94ec7ea499ef92ae0c5890b618
Uma Maheswari Paladugu <umamaheswari6336@gmail.com>
2022-06-05 07:47:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source = "./modules/vpc"
+  region = var.region
+}
+
+resource "aws_instance" "my-instance" {
+  ami           = module.vpc.ami_id
+  subnet_id     = module.vpc.subnet_id
+  instance_type = "t2.micro"
+}
